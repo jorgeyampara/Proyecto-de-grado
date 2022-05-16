ac2d79b44929aa3b3615a5cdc1a8c7b209b3ccaa
jorge <--jorgeyampara24@gmail.com>
2022-05-16 15:47:24 -0400

    codigo sin firebase

---
+++ codigo android/app/src/main/java/com/jorgeyampara/raicesnuevo/LoginActivity.java
@@ -0,0 +1,93 @@
+package com.jorgeyampara.raicesnuevo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText Correo;
+    private EditText Contraseña;
+    private Button botonIniciarSecion;
+    private Button botonCrearCuenta;
+    private Button reestablecerContraseña;
+    //variables de los datos del usuario
+
+    private String name;
+    private String email;
+    private String password;
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Correo = (EditText) findViewById(R.id.correoElectronico);
+        Contraseña = (EditText) findViewById(R.id.contraseña);
+        botonCrearCuenta = (Button) findViewById(R.id.crearCuenta);
+        botonIniciarSecion = (Button) findViewById(R.id.iniciarSecion);
+        reestablecerContraseña = (Button) findViewById(R.id.reestablecerPassword);
+
+
+        //boton crear cuenta
+        botonCrearCuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(com.jorgeyampara.raices.LoginActivity.this, RegistrarseActivity.class));
+            }
+        });
+
+
+        //boton reestablecer contraseña
+        reestablecerContraseña.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(com.jorgeyampara.raices.LoginActivity.this, ResetPassword.class));
+            }
+        });
+
+        //boton inisiar sesion
+        botonIniciarSecion.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                email= Correo.getText().toString();
+                password = Contraseña.getText().toString();
+
+                if (!email.isEmpty() && !password.isEmpty()){
+                    FirebaseAuth.getInstance().signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if (task.isSuccessful()){
+                                startActivity(new Intent(com.jorgeyampara.raices.LoginActivity.this, MainActivity.class));
+                                finish();
+                            }
+                            else {
+                                Toast.makeText(com.jorgeyampara.raices.LoginActivity.this, "Usuario o contraseña incorrecto",Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    });
+                }
+                else {
+                    Toast.makeText(com.jorgeyampara.raices.LoginActivity.this, "Debe completar todos los campos",Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+        );
+
+    }
+}
---
+++ codigo modulo wifi/espFirebase/espFirebase.ino
@@ -0,0 +1,88 @@
+/**********************************************************************************
+ * TITULO: Código ESP8266 - FIREBASE
+ * AUTOR: Jhimmy Astoraque Durán
+ * DESCRIPCION: Este es el código de este episodio del canal que tambien es parte de la Serie Intermedia del esp8266
+ * CANAL YOUTUBE: https://www.youtube.com/c/jadsatv
+ * © 2020
+ * *******************************************************************************/
+
+#include <ESP8266WiFi.h>
+#include "FirebaseESP8266.h"
+
+// Credenciales wifi
+const char *ssid = "1"; // nombre de la red
+const char *password = "123456789";
+//const char *ssid = "ZTE-d794ca"; // nombre de la red
+//const char *password = "AMERICANO1";
+
+// Credenciales Proyecto Firebase
+const char *FIREBASE_HOST = "raices-68dc9-default-rtdb.firebaseio.com";
+const char *FIREBASE_AUTH = "etxcXlTcE034EpEoOyLLQaWCbFoTORP2AD6gGwof";
+
+// Firebase Data object in the global scope
+FirebaseData firebaseData;
+
+bool iterar = true;
+
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(250);
+  }
+  Serial.print("\nConectado al Wi-Fi");
+  Serial.println();
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+  pinMode(16, OUTPUT);
+}
+
+void loop()
+{
+  String nodo = "Proyecto1";
+
+  //while (iterar)
+  //{
+
+    // escribir datos
+   Firebase.setInt(firebaseData, "lectura1", 512);
+   Firebase.setString(firebaseData, "string1", "On");
+
+    Firebase.setString(firebaseData, nodo + "/sensor", "DHT11");
+    //Firebase.setInt(firebaseData, nodo + "/temperatura", 31);
+    //Firebase.setBool(firebaseData, nodo + "/alarmas", false);
+
+    // leer datos
+     Firebase.getInt(firebaseData,"luz");
+     //Serial.println(firebaseData.intData());
+     if ( firebaseData.intData() == 1){
+      digitalWrite(16, HIGH);
+     }
+     else{
+      digitalWrite(16, LOW);
+     }
+     delay(250);
+     //Firebase.getString(firebaseData, nodo + "/sensor");
+    // Serial.println(firebaseData.stringData());
+
+    // push de datos
+    //  Firebase.pushInt(firebaseData, nodo + "/temperatura", 29);
+    //  delay(150);
+    //  Firebase.pushInt(firebaseData, nodo + "/temperatura", 31);
+    //  delay(150);
+    //  Firebase.pushInt(firebaseData, nodo + "/temperatura", 30);
+    //  delay(150);
+    //  Firebase.pushInt(firebaseData, nodo + "/Humedad", 67);
+
+   // iterar = false;
+    //Firebase.end(firebaseData);
+  //}
+} // End Loop
